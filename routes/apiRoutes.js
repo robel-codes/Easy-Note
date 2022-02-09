367f913b85f007fb2c1eb25bee353a8beb3533b8
Robel <rofikre@yahoo.com>
2022-02-09 14:21:57 -0500

    set up Get routes and created Post routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,38 @@
+let db = require("../db/db.json");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+getMaxID = (notes) => {
+  return notes[0].current_id;
+}
+
+let id = {current_id:getMaxID(db)};
+
+module.exports = function(app) {
+
+  app.use(express.json());
+  app.use(express.static(path.join(__dirname, '../')))
+
+  // GET 
+  app.get("/api/notes", function(req, res) {
+    console.log(db);
+    res.json(db.slice(1));
+    });
+
+  // POST
+  app.post("/api/notes", function(req, res) {
+    id.current_id++; 
+    req.body.id = id.current_id; 
+    console.log(req.body);
+    db.push(req.body);
+    db[0].current_id++
+    fs.appendFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db), 'utf8', (err) => {
+      if (err) throw err;
+      else console.log("file written successfully");
+    });
+    console.log(db);
+    res.json(db.slice(1));
+  });
+
+}
